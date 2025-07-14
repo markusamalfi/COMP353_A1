67dcc03b1360a6ad721f49db2885136500c94707
Markus A. <103454069+markusamalfi@users.noreply.github.com>
2025-07-14 18:47:35 -0400

    Part 1, e)

---
+++ Part1/Part1E.sql
@@ -0,0 +1,9 @@
+SET SQL_SAFE_UPDATES = 0;
+
+-- Delete all rows from DonorInterests
+DELETE FROM DonorInterests;
+
+-- Delete all rows from Categories
+DELETE FROM Categories;
+
+SET SQL_SAFE_UPDATES = 1; 
